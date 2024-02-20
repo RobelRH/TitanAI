c9378c7a4ae217736134faa0d01a906b308b0f90
RobelRH <robel.redda28@gmail.com>
2024-02-20 23:15:30 +0300

    image form implemented

--- app/api/webhooks/clerk/routes.ts
+++ app/api/webhooks/clerk/routes.ts
@@ -59,6 +59,7 @@ export async function POST(req: Request) {
 
   // CREATE
   if (eventType === "user.created") {
+    console.log("user created event reached")
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
