51984bd40d6bb90f2de85a9ca086529dd1512f2f
RobelRH <robel.redda28@gmail.com>
2024-03-03 20:21:06 +0300

    update file name routes.ts to route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -8,6 +8,9 @@ import { Webhook } from "svix";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
 export async function POST(req: Request) {
+
+  console.log("POST event reached")
+
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
@@ -59,7 +62,6 @@ export async function POST(req: Request) {
 
   // CREATE
   if (eventType === "user.created") {
-    console.log("user created event reached")
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
